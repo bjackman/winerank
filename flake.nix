ced40ceb021b3f7771f73232d3db5170ba9ccbe4
Brendan Jackman <bhenryj0117@gmail.com>
2026-05-25 13:05:51 +0200

    flake: Unpin nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,10 +2,6 @@
   description = "winerank — YouTube transcript fetcher + realwines.ch scraper";
 
   inputs = {
-    # TODO: This is pinned to the same rev as ~/src/boxen to avoid re-downloading
-    # nixpkgs on a slow connection. Replace with "github:nixos/nixpkgs/nixos-unstable"
-    # and run `nix flake update` once you're on a faster connection.
-    nixpkgs.url = "github:nixos/nixpkgs/ed67bc86e84e51d4a88e73c7fd36006dc876476f";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
